4913e544de32e92fcf341cd2be0d31403c49beba
rotfg17 <robinsonchalasj@gmail.com>
2023-11-01 15:02:29 -0400

    test git

---
+++ terminosycondiciones.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/81581fb069.js" crossorigin="anonymous"></script>
+        <link rel="icon" type="image/png" sizes="128x128"  href="img/favicon.ico">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <link rel="stylesheet" href="css/css.css">
+        <title>Ferre Seibo - Términos y Condiciones</title>
+    </head>
+<body>
+    <div class="sidebar">
+        <div class="hdn-head">
+            <a href="#" ><h2>Hola, Inicia sesión</h2></a>
+        </div>
+        <div class="hdn-content">
+            <h3>Todas las categorías</h3>
+            <ul>
+                <div>
+                    <a href="herraminetas.html"><li>Herramientas</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                  <a href="herreria.html"> <li>Herreria</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                    <a href="jardineria.html"><li>Jardinería</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                   <a href="quimicos.html"><li>Químico</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                   <a href="madera.html"><li>Madera</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                   <a href="refrigeracion.html"><li>Refrigeración</li></a> <i class="fa-solid fa-angle-right"></i>
+                </div>
+
+                <div>
+                    <a href="pintura.html"><li>Pintura</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+
+                <div>
+                 <a href="pinturasking.html"><li>Pinturas King</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+
+                <div>
+                   <a href="ebanisteria.html"><li>Ebanisteria</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                  <a href="plomeria.html"><li>Plomería</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                   <a href="cerrajeria.html"><li>Cerrajería</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                  <a href="accesoriosbano.html"><li>Accesorios de Baño</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                
+                <div>
+                   <a href="construccion.html"> <li>Construcción Ligera</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                    <a href="agregados.html"><li>Agregados</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                  <a href="accesoriospintura.html"><li>Accesorios de Pintura</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                   <a href="productosgenerales.html"><li>Productos Generales</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+                <div>
+                  <a href="electricidad.html"><li>Electricidad</li></a><i class="fa-solid fa-angle-right"></i>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <i class="fa-solid fa-xmark"></i>
+    <div class="triangle"></div>
+        <div class="hdn-sign">
+            <div class="hdn-table">
+                <div>
+                <h3>
+                    Tu Lista
+                </h3>
+                <ul>
+                    <li>Crear una lista</li>
+                </ul>
+            </div>
+            <div class="hdn-line"></div>
+            <div>
+                <h3>Tu cuenta</h3>
+                <ul>
+                    <li>Iniciar sesión</li>
+                    <li>Mi perfil</li>
+                    <li>Mis pedidos</li>
+                   <a href="acercade.html"><li>Nuestra tienda</li></a> 
+                    <a href="https://wa.me/c/18095523071" target="_blank"><li>Contáctanos</li></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="black"></div>
+   <header>
+    <div class="first">
+        <div class="flex logo">
+       <a href="index.html"><img src="img/Logo.png"></a> 
+        </div>
+        <div class="flex input">
+            <input type="text" placeholder="Buscar">
+            <i class="fas fa-search"></i>
+        </div>
+    <div class="flex right">
+       <div class="sign">
+        <span>Hola, Inicia sesión</span>
+            <div class="flex ac">
+                <span>Mi cuenta y pedidos</span>
+                <i class="fa-solid fa-chevron-down"></i>
+            </div>
+       </div>
+       <div class="flex cart">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="ca">Carrito</span>
+                <p>0</p>
+            </div>
+        </div>
+    </div>
+    <div class="second">
+        <div class="second-1">
+            <div>
+                <i class="fas fa-bars"></i>
+                <p class="menu-icon">Menú</p>
+                <span>Todas las categorías</span>
+                <i class="fa-solid fa-chevron-down"></i>
+            </div>
+        </div>
+        <div class="second-2">
+            <ul>
+                <i class="fa-solid fa-headset"></i>
+                <a href="https://wa.me/c/18095523071" target="_blank"><li>Servicio al cliente</li></a>
+              <i class="fa-solid fa-book-open"></i>
+              <a href="blog.html"><li>Blogs</li></a>
+              <i class="fa-solid fa-phone"></i>
+              <a href="tel:8095523071" target="_blank"><li>Contacto</li></a>
+            </ul>
+        </div>
+    </div>
+   </header>  
+
+   <div class="terminos-container">
+    <h1 class="title">
+        TÉRMINOS Y CONDICIONES DE USO DEL SITIO WEB DE FERRETERÍA FERRE SEIBO
+        <span>La ferreteria del pueblo</span>
+    </h1>
+    <div class="content">
+        <h3>Bienvenidos a Ferre Seibo</h3>
+        <p>El uso de Ferre Seibo está sujeto a los siguientes Términos y Condiciones de uso que deberán ser cumplidos por nuestros clientes y usuarios.</p>
+        <p>Estos Términos y Condiciones podrán variar en cualquier momento, reflejándose dichos cambios periódicamente en nuestra web. Cada vez que ingreses, deberás leer cuidadosamente los Términos y Condiciones ya que para todos los efectos legales y por el sólo ingreso a los contenidos de Ferre Seibo darás por aceptados los presentes Términos y Condiciones, y sus modificaciones.</p>
+        <p>Toma en cuenta que la información proporcionada por otros sitios web, especialmente cuando el acceso al sitio www.ferreseibo.com se haya efectuado a través de un link o banner en sitios que no dispongan de contratos o alianzas con nosotros, es bajo tu responsabilidad.</p>
+        <p>Podremos modificar cualquier información contenida en la tienda virtual, incluyendo las relacionadas con productos, servicios, precios, stocks y condiciones, así como interrumpir el servicio de la tienda virtual en forma transitoria o permanente, total o parcial, en cualquier momento y sin previo aviso.</p>
+        
+        <h3>Aceptación de los Términos y Condiciones</h3>
+        <p>Al acceder y utilizar el sitio web de Ferretería Ferre Seibo (en adelante, el "Sitio Web"), usted acepta cumplir y estar sujeto a los siguientes términos y condiciones de uso. Si no está de acuerdo con estos términos y condiciones, le recomendamos que no utilice el Sitio Web.</p>
+
+        <h3>Uso del Sitio Web</h3>
+        <p> El Sitio Web está destinado únicamente para uso personal y no comercial. No puede utilizar el Sitio Web con fines comerciales sin el consentimiento expreso de Ferretería Ferre Seibo.</p>
+        <p>Usted se compromete a utilizar el Sitio Web de manera legal y ética, y a cumplir con todas las leyes y regulaciones aplicables.</p>
+        <p>No está permitido utilizar el Sitio Web para transmitir, distribuir o almacenar material que sea ilegal, difamatorio, ofensivo, o que viole los derechos de propiedad intelectual de terceros.</p>
+        
+        <h3>Propiedad Intelectual</h3>
+        <p>Todo el contenido del Sitio Web, incluyendo, pero no limitado a, textos, gráficos, logotipos, imágenes, videos, iconos y software, está protegido por derechos de autor y otros derechos de propiedad intelectual de Ferretería Ferre Seibo o sus licenciantes.</p>
+        <p>Usted no tiene permiso para copiar, reproducir, distribuir, transmitir, modificar, crear obras derivadas o utilizar de cualquier manera el contenido del Sitio Web sin el consentimiento previo por escrito de Ferretería Ferre Seibo.</p>
+
+        <h3>Privacidad y Datos Personales</h3>
+        <p>La política de privacidad del Sitio Web establece cómo se recopilan, utilizan y protegen sus datos personales. Al utilizar el Sitio Web, usted acepta las prácticas de privacidad descritas en dicha política.</p>
+
+        <strong>Limitación de Responsabilidad</strong>
+        <p>Ferretería Ferre Seibo no se hace responsable de ningún daño directo, indirecto, incidental, especial o consecuente que pueda resultar del uso o la incapacidad de uso del Sitio Web, incluyendo, pero no limitado a, la pérdida de datos, la interrupción del negocio o cualquier otro daño financiero o personal.</p>
+
+
+
+        <h3>PROCESO DE COMPRA</h3>
+        <p>A través de este sitio web la empresa realiza ofertas de bienes y servicios (“Productos”), que pueden ser aceptadas por vía electrónica, y utilizando los mecanismos que el mismo sitio ofrece para ello. Una vez que hayamos recibido tu pedido, validaremos el método de pago que hayas utilizado y haremos el cargo correspondiente. De ser el caso, te enviaremos una comunicación confirmando que tu compra ha sido realizada satisfactoriamente; o en su defecto, indicando que no ha podido ser procesada.</p>
+        <p>Ante cualquier pedido de anulación o cancelación, que haya sido realizado por el titular de la compra o el titular del medio de pago usado para esta y que se encuentre acorde a nuestras políticas, procederemos a comunicar este hecho a la entidad financiera emisora de la tarjeta que hayas utilizado en tu solicitud de compra. Dicha entidad financiera será la única responsable de proceder con la liberación del importe retenido para la compra o la devolución del importe cobrado, de acuerdo con sus procedimientos y políticas correspondientes.</p>
+        
+        <h3>Stock y productos</h3>
+        <p>La disponibilidad de los productos a elegir a través de nuestra web estará sujeta a la disponibilidad de stock de la tienda virtual, el hecho de que un producto aparezca publicado en el sitio no necesariamente implica que exista stock disponible del mismo. Dicho stock será reservado solo si ya cuentas con la aprobación de tu compra online, la cual te será oportunamente comunicada.</p>
+        <p>Las imágenes y colores de los productos en la página web son referenciales y pueden variar con respecto al producto físico. Las imágenes también pueden presentar al producto instalado en un ambiente para mostrar el acabado y funcionalidad del mismo, sin que esto indique que el producto se venda en conjunto.</p>
+        
+        <h3>Forma de pago</h3>
+        <p>Las tarjetas de crédito aceptadas son las siguientes: Tarjeta Visa, Mastercard y American Express emitidas por bancos nacionales. No será aceptada ninguna tarjeta cerrada (aquella que no cuenta con el logo de Mastercard, Visa o American Express).</p>
+        <p>Las tarjetas de débito aceptadas son las siguientes: Visa y Mastercard; emitidas por bancos nacionales.</p>
+        <p>También podrás pagar en efectivo.</p>
+
+        <h3>SEGURIDAD WEB</h3>
+        <p>Tus datos personales y toda tu información se encuentran protegidos con el Certificado de seguridad SSL Vtex.</p>
+        <strong>¿Qué es un certificado de seguridad?</strong>
+        <p>Al acceder a un sitio web que utiliza HTTPS (seguridad de conexión), el servidor utiliza un certificado para demostrar la identidad del sitio web a los navegadores (como Chrome). Para que puedas navegar por la Web de forma segura, Chrome solicita a los sitios web que utilicen certificados de organizaciones de confianza.</p>
+        <strong>¿Por qué el Certificado SSL Vtex?</strong>
+        <p>El protocolo SSL (Secure Socket Layer) es un protocolo desarrollado para aumentar la seguridad de los datos transmitidos a través de Internet. Las conexiones a través de SSL se recomiendan especialmente para el envío de información tal como números de tarjetas de crédito, contraseñas y cualquier otra información sensible a través de Internet.</p>
+        <p>Siempre que se acceda a una página segura que está protegida por un certificado SSL, una llave o un candado aparece en la barra de estado para indicar una comunicación segura. El Certificado SSL Vtex, es uno de los más conocidos y manejados a nivel mundial, brindando total seguridad en tu transacción a través de nuestra Tienda Virtual.</p>
+        
+        <h3>LEY APLICABLE Y JURISDICCION</h3>
+        <p>Ferre Seibo tiene su sede en la ciudad del Seibo, República Dominicana. Todos los asuntos legales que surjan o estén relacionados con el sitio ferre Seibo, deberán ser atendidos con y según lo determinado por la legislación Dominicana. El Cliente acepta someterse a la jurisdicción de los tribunales de República Dominicana, en cualquier proceso o acción legal, sin perjuicio de las demás autoridades Dominicana que pudiesen resultar competentes.</p>
+        <p>Ferre Seibo se reserva el derecho de denegar o retirar el acceso al sitio Promart.pe y/o a los servicios que en él se ofrecen y prestan, en cualquier momento y sin necesidad de preaviso, a aquellos Clientes que incumplan estos Términos y Condiciones y/o las condiciones particulares que resulten de aplicación. Si alguna disposición aquí contenida es declarada nula o no ejecutable por alguna autoridad competente, ello no afectará la validez y eficacia de las disposiciones restantes, las mismas que mantendrán plena vigencia.</p>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="contenido contenido-footer">
+        <div class="menu-footer">
+            <div class="contact-footer">
+                <p class="title-footer">Información de Contacto</p>
+                <ul>
+                    <li>Dirección: C/Sánchez, esquina Mella en los Hoyitos, 
+                        Santa Cruz de El Seibo, República Dominicana.
+                    </li>
+                    <li>Teléfono:809-552-3071 / 809-552-3071</li>
+                    <li>Correo Electronico: contabilidadferreseibo@gmail.com</li>
+                </ul>
+                <div class="social-icons">
+                    <span class="facebook"><i class="fa-brands fa-facebook-f"></i></span>
+                    <span class="twitter"><i class="fa-brands fa-twitter"></i></span>
+                    <span class="instagram"><i class="fa-brands fa-instagram"></i></span>
+                </div>
+            </div>
+
+            <div class="information">
+                <p class="title-footer">Información</p>
+                <ul>
+                    <li><a href="acercade.html">Acerca de Nosotros</a></li>
+                    <li><a href="politica.html">Politicas de Privacidad</a></li>
+                    <li><a href="terminosycondiciones.html">Términos y condiciones</a></li>
+                    <li><a href="contactanos.html">Contactános</a></li>
+                </ul>
+            </div>
+            <div class="mi-cuenta">
+                <p class="title-footer">Mi cuenta</p>
+                
+                    <ul>
+                        <li><a href="#">Mi cuenta</a></li>
+                        <li><a href="#">Historial de ordenes</a></li>
+                        <li><a href="#">Lista de deseos</a></li>
+                        <li><a href="#">Boletin</a></li>
+                    </ul>
+            </div>
+            <div class="newsletter">
+                <p class="title-footer">Boletín Informativo</p>
+                <div class="contenido">
+                    <p>
+                        Suscribete a nuestros boletines ahora y mantente al
+                        día con nuevas colecciones y ofertas exclusivas.
+                    </p>
+                    <input type="email" placeholder="Ingresa el correo aquí...">
+                    <button>Suscríbete</button> 
+                </div>
+            </div>
+        </div>
+
+     <div class="copyright">
+        <p>
+            Desarrollado por Robinson Chalas y Noel Roja &copy; 2023
+        </p>
+     </div>   
+    </div>
+</footer>
+
+   <script src="js/script.js"></script>
+<script src="js/randomize.js"></script>
+<script src="js/index.js"></script>   
+<script src="js/apps.js"></script>
+</body>
+</html>
